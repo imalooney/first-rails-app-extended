780a3d476d9912724b2e365ac97abc7a9457a993
MakerSquare Student <students@themakersquare.com>
2013-12-02 15:59:12 -0600

    everything goes through the show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,40 +1,63 @@
-class MoviesController < ApplicationController
-  
-  def show_gladiator
-    @movie = {
-      title: 'Gladiator',
-      subtitle: "Gladiator 'Are You Not Entertained' Scene",
-      video_id: 'FI1ylg4GKv8',
-      description: "Crowe shows everyone what's up."
+class MoviesController < ApplicationController 
+  def show
+    movie_name = params[:movie_name].downcase
+    movies ={
+      "gladiator" =>{
+          title: 'Gladiator',
+          subtitle: "Gladiator 'Are You Not Entertained' Scene",
+          video_id: 'FI1ylg4GKv8',
+          description: "Crowe shows everyone what's up."
+        },
+      "matilda" => {
+        title: 'Matilda',
+        subtitle: "Big kid eating a brownie",
+        video_id: 'EVWOQwZENBg',
+        description: "Crowe shows everyone what's up."
+      }
     }
+    @movie = movies[movie_name]
+    # @params = params
   end
 
-  def show_matilda
-    @movie = {
-      title: 'Matilda',
-      subtitle: "Big kid eating a brownie",
-      video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
-    }  
-  end
-  def show_dreamgirls
-    @movie = {
-      title: 'Dreamgirls',
-      subtitle: "Big kid eating a brownie",
-      video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
-    }  
-  end
-
-  def show_austinpowers3
-    @movie = {
-      title: 'Austin Powers Gold Member',
-      subtitle: "Oh, behave~!",
-      video_id: 'SwB0ZJbwmpw',
-      description: "Yeah baby!"
-    } 
-  end
   def show_all
-    
+    @params = params
   end
+
 end
+
+# def show_gladiator
+#   @movie = {
+#     title: 'Gladiator',
+#     subtitle: "Gladiator 'Are You Not Entertained' Scene",
+#     video_id: 'FI1ylg4GKv8',
+#     description: "Crowe shows everyone what's up."
+#   }
+# end
+
+# def show_matilda
+#   @movie = {
+#     title: 'Matilda',
+#     subtitle: "Big kid eating a brownie",
+#     video_id: 'EVWOQwZENBg',
+#     description: "Quite the random movie, but a classic for sure!"
+#   }  
+#   @params = params
+# end
+# def show_dreamgirls
+#   @movie = {
+#     title: 'Dreamgirls',
+#     subtitle: "Big kid eating a brownie",
+#     video_id: 'EVWOQwZENBg',
+#     description: "Quite the random movie, but a classic for sure!"
+#   }  
+# end
+
+# def show_austinpowers3
+#   @movie = {
+#     title: 'Austin Powers Gold Member',
+#     subtitle: "Oh, behave~!",
+#     video_id: 'SwB0ZJbwmpw',
+#     description: "Yeah baby!"
+#   } 
+# end
+# 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 MetubeCohort3::Application.routes.draw do
-
+	get '/movies/:movie_name' => 'movies#show', as: 'movie'
+	
 	root "movies#show_all"
   get '/movies' => 'movies#show_all'
-  get '/movies/gladiator' => 'movies#show_gladiator', as: 'gladiator'
-  get '/movies/matilda' => 'movies#show_matilda', as: 'matilda'
-  get '/movies/austinpowers3' => 'movies#show_austinpowers3', as: 'austinpowers3'
-  get '/movies/dreamgirls' => 'movies#show_dreamgirls', as: 'dreamgirls'
-  
 
 end
 
 
-# get '/shows/seinfeld' => 'shows#show_seinfeld'
+# 	get '/shows/seinfeld' => 'shows#show_seinfeld'
 #   get '/shows/friends' => 'shows#show_friends'
 #   get '/shows/alias' => 'shows#show_alias'
+# get '/movies/gladiator' => 'movies#show_gladiator', as: 'gladiator'
+#   get '/movies/matilda' => 'movies#show_matilda', as: 'matilda'
+#   get '/movies/austinpowers3' => 'movies#show_austinpowers3', as: 'austinpowers3'
+#   get '/movies/dreamgirls' => 'movies#show_dreamgirls', as: 'dreamgirls'
